92800e7ed55f3b5097bdc700a1df1acc33f68ffb
kimgun95 <harrison1995@naver.com>
2023-01-24 17:31:58 +0900

    1. socket을 통해 outputstream을 받는다. 2. outputstream을 통해 key, value를 write 3. inputstream을 통해 read가 잘되는지 확인한다, 0은 정상적으로 받았다는 의미

--- src/test/java/tyrant/TyrantTest.java
+++ src/test/java/tyrant/TyrantTest.java
@@ -3,7 +3,12 @@ package tyrant;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class TyrantTest {
 
@@ -12,6 +17,23 @@ public class TyrantTest {
 //    Tyrant tyrant = new Tyrant();
 //    tyrant.put("key", "value");
 //    assertThat(t.get("key"), is("value"));
-    new Socket("localhost", 1978);
+
+    Socket s = new Socket("localhost", 1978);
+    OutputStream writer = s.getOutputStream();
+    writer.write(0xC8); // operation prefix
+    writer.write(0x10); // put operation
+    writer.write(0);
+    writer.write(0);
+    writer.write(0);
+    writer.write(3); // 4 byte
+    writer.write(0);
+    writer.write(0);
+    writer.write(0);
+    writer.write(5); // 4 byte
+    writer.write(new byte [] {'k', 'e', 'y'}); // key
+    writer.write(new byte [] {'v', 'a', 'l', 'u', 'e'}); // value
+    InputStream reader = s.getInputStream();
+    int status = reader.read();
+    assertThat(status, is(0));
   }
 }
