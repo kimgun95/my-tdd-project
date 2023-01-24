49e64c3e7d2e7eb0d979c41011f4ebc9bf532e97
kimgun95 <harrison1995@naver.com>
2023-01-24 18:06:00 +0900

    1. 4byte의 write를 DataOutPutStream을 이용해서 writeInt로 바꿔준다, 값의 길이도 length 이용 2. socket 인터페이스는 open, close가 있어야 함. socket, writer, reader를 open 메서드로 추출 3. key, value는 로컬 변수가 아닌 parameter로 전달되기 때문에 extract 4. tyrantmap을 변수로 추출 후 open, close 메서드를 밖에서 호출하게 함

--- src/test/java/tyrant/TyrantTest.java
+++ src/test/java/tyrant/TyrantTest.java
@@ -2,9 +2,9 @@ package tyrant;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,7 +19,10 @@ public class TyrantTest {
 //    tyrant.put("key", "value");
 //    assertThat(t.get("key"), is("value"));
 
-    new TyrantMap().put();
+    TyrantMap tyrantMap = new TyrantMap();
+    tyrantMap.open();
+    tyrantMap.put("key", "value");
+    tyrantMap.close();
   }
 
   private class TyrantMap {
@@ -27,29 +30,29 @@ public class TyrantTest {
     public static final int OPERATION_PREFIX = 0xC8;
     public static final int PUT_OPERATION = 0x10;
     private Socket socket;
-    private OutputStream writer;
+    private DataOutputStream writer;
     private InputStream reader;
 
-    public void put() throws IOException {
-      String key = "key";
-      String value = "value";
-      socket = new Socket("localhost", 1978);
-      writer = socket.getOutputStream();
+    public void put(String key, String value) throws IOException {
+
       writer.write(OPERATION_PREFIX);
       writer.write(PUT_OPERATION);
-      writer.write(0);
-      writer.write(0);
-      writer.write(0);
-      writer.write(3); // 4 byte
-      writer.write(0);
-      writer.write(0);
-      writer.write(0);
-      writer.write(5); // 4 byte
+      writer.writeInt(key.length());
+      writer.writeInt(value.length());
       writer.write(key.getBytes());
       writer.write(value.getBytes());
-      reader = socket.getInputStream();
       int status = reader.read();
       assertThat(status, is(0));
+    }
+
+    private void open() throws IOException {
+      socket = new Socket("localhost", 1978);
+      writer = new DataOutputStream(socket.getOutputStream());
+      reader = socket.getInputStream();
+    }
+
+    public void close() throws IOException {
+      socket.close();
     }
   }
 }
