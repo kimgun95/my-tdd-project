4ce6552ef80cc868dc3dd6b4afef3ac29663ff57
kimgun95 <harrison1995@naver.com>
2023-01-24 17:14:08 +0900

    1. start with high level test 2. comment out 3. 제일 쉽고, 바로 실행해 볼 수 있는 일로 시작

--- src/test/java/tyrant/TyrantTest.java
+++ src/test/java/tyrant/TyrantTest.java
@@ -8,7 +8,10 @@ import java.net.Socket;
 public class TyrantTest {
 
   @Test
-  public void nothing() throws IOException {
+  public void tyrant에put한뒤get하기() throws IOException {
+//    Tyrant tyrant = new Tyrant();
+//    tyrant.put("key", "value");
+//    assertThat(t.get("key"), is("value"));
     new Socket("localhost", 1978);
   }
 }
